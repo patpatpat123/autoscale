d3d542335c1f4ff2bf44b0daee23b1ff59edeedd
patpatpat123 <43899031+patpatpat123@users.noreply.github.com>
2021-08-05 15:28:04 +0800

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @SpringBootApplication
 @RestController
 public class Application {
@@ -20,7 +22,17 @@ public class Application {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication application = new SpringApplication(Application.class);
+		Map<String, Object> properties = Map.of("logging.level.root", "DEBUG",
+				"management.endpoint.env.enabled", false,
+				"management.endpoint.health.group.liveness.include", "*",
+				"management.endpoint.health.group.readiness.include", "*",
+				"management.endpoint.health.show-details", "always",
+				"management.endpoints.web.base-path", "/actuator",
+				"management.endpoints.web.exposure.include", "*",
+				"management.endpoints.web.path-mapping.health", "/health");
+		application.setDefaultProperties(properties);
+		application.run(args);
 	}
 
 }
